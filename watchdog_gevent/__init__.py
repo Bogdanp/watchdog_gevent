00dffcd7cab6e51306a887956f67a1bff67f0db5
Bogdan Popa <bogdan@defn.io>
2024-04-29 15:53:43 +0300

    chore: bump version 0.1.1 → 0.2.0

--- watchdog_gevent/__init__.py
+++ watchdog_gevent/__init__.py
@@ -10,7 +10,7 @@ except (ImportError, RuntimeError) as e:  # pragma: no cover
     warnings.warn(str(e), ImportWarning, stacklevel=2)
 
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __all__ = [
     "GeventEmitter",
     "GeventObserver",
